e9123d35eb907b30a562df6caecf8fd5ed7171ad
dtemilade <dtemilade@gmail.com>
2023-06-12 11:51:11 -0700

    corrections on mandatory task for 0x03. Python - Data Structures: Lists, Tuples project

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,11 +2,9 @@
 """function that prints all integers of a list, in reverse order"""
 
 
-def print_reversed_list_integer(my_list=[])
-
-
-if isinstance(my_list, list):
-    my_list.reverse()
+def print_reversed_list_integer(my_list=[]):
+    if isinstance(my_list, list):
+        my_list.reverse()
 
     for listvar in my_list:
         print("{:d}".format(listvar))
