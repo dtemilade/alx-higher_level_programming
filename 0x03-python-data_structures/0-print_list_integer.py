60ef7d807856ead7869e4d90c7205fcd38583d79
dtemilade <dtemilade@gmail.com>
2023-06-12 05:42:19 -0700

    updating changes on task

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """function that prints all integers of a list."""
 def print_list_integer(my_list=[]):
+    """conditional statement for the function"""
     for listvar in range(len(my_list)):
         print("{:d}".format(my_list[listvar]))
