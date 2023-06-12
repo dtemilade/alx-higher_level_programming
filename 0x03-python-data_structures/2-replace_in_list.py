c73f20f10dd64512ce3d9d081d1afee8ca18fec9
dtemilade <dtemilade@gmail.com>
2023-06-12 11:39:04 -0700

    corrections on mandatory task for 0x03. Python - Data Structures: Lists, Tuples project

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,7 +6,5 @@ def replace_in_list(my_list, idx, element):
     """idx is negative or out of range, return None"""
     if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
-        """Output the result"""
-        return my_list
-    else:
-        return None
+    """Output the result"""
+    return (my_list)
