8c39aeea650b8db65d6eb047751ff731ad2c8d06
dtemilade <dtemilade@gmail.com>
2023-07-05 15:39:49 -0700

    correction commits

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -42,9 +42,9 @@ class Rectangle:
 
     def perimeter(self):
         """Display result for perimeter of the Rectangle."""
-        if self.__height == 0 or self.__width == 0:
+        if self.__width == 0 or self.__height == 0:
             return (0)
-        return ((self.__height * 2) + (self.__width * 2))
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """Display result for Rectangle with the # character."""
@@ -53,10 +53,11 @@ class Rectangle:
 
         retval = []
         for x in range(self.__height):
-            [retval.append('#') for y in range(self.__width)]
+            [retval.append('#')
+                    for y in range(self.__width)]
             if x != self.__height - 1:
                 retval.append("\n")
-                return ("".join(retval))
+        return ("".join(retval))
 
     def __repr__(self):
         """Display result for string representation of the Rectangle."""
