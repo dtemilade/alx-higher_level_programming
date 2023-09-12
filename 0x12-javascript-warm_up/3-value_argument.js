b15459b2950b8c026594ce225eb272c4be72efef
dtemilade <dtemilade@gmail.com>
2023-09-11 19:57:26 -0700

    first submission commit

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// script that prints the first argument passed to it:
+
+#!/usr/bin/node
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+
+} else {
+  console.log('No argument');
+}
