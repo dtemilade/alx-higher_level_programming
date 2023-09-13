5023cdb89c1a74075dbd49137768aa7b13c724af
dtemilade <dtemilade@gmail.com>
2023-09-12 22:00:23 -0700

    other commits

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// function that prints the number of arguments already printed
+// and the new argument value
+
+let num = -1;
+exports.logMe = function (item) {
+  num++;
+  console.log(num + ': ' + item);
+};
