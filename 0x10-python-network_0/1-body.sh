26996f9d709c2b98d6a6f9db2af2dabb2603094d
dtemilade <dtemilade@gmail.com>
2023-09-28 16:12:25 -0700

    self

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 # Bash script for URL to send GET-request and display RESPONSE
 
-curl -sL $1
+#curl -sL $1
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+URL=$1
+
+curl_response=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
+
+if [ "$curl_response" -eq 200 ]; then
+  echo "Response body for $URL (HTTP 200 OK):"
+
+  curl -s "$URL"
+else
+  echo "$curl_response"
+fi
