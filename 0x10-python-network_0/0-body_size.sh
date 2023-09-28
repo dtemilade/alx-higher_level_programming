64c09d3c90982375eeec5902f3e8b7cbd870c823
dtemilade <dtemilade@gmail.com>
2023-09-28 12:24:56 -0700

    updating

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # Bash script for URL to send REQUEST and display RESPONSE
 
-curl -Is $1 | awk '/Content-Length/{print $2}'
+if [ -z "$1" ]; then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+
+URL=$1
+
+RETVAL=$(curl -sI "$URL" | awk '/Content-Length/ {print $2}')
+echo $RETVAL
