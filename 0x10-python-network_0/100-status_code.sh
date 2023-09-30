50a11a1173a201b3608473cb83118039fd5e6279
dtemilade <dtemilade@gmail.com>
2023-09-30 07:46:13 -0700

    corrections updated

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #  Bash script to send request of URL passed as an argument, and displays status code.
-
 curl -s $1 -o /dev/null -w '%{http_code}'
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Bash script that send REQUEST to a SERVER and display RESPONSE
-
-url -s http://0.0.0.0:5000/catch_me > response.txt
-less response.txt
+url -s http://0.0.0.0:5000/catch_me > response.txt less response.txt
