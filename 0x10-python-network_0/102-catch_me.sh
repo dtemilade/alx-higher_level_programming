3797140ce887d43ce80122323ec5a776bb6aa81c
dtemilade <dtemilade@gmail.com>
2023-09-30 07:49:25 -0700

    corrections updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that send REQUEST to a SERVER and display RESPONSE
-url -s http://0.0.0.0:5000/catch_me > response.txt less response.txt
+curl -s 0.0.0.0:5000/catch_me -X POST -d "You got me!" #url -s http://0.0.0.0:5000/catch_me > response.txt less response.txt
