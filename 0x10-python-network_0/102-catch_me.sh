665fc1e37b5a315a207fbd7a2d2d647e6db1344a
dtemilade <dtemilade@gmail.com>
2024-01-10 04:06:44 -0800

    updating commits

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that send REQUEST to a SERVER and display RESPONSE
-curl -sd 'user_id=98' -X PUT -H 'Origin: School' -L 0.0.0.0:5000/catch_me
+# Bash script that send REQUEST to a SERVER and display RESPONS
+curl -s -X PUT -H "Origin: HolbertonSchool" -L -d "user_id=98" 0.0.0.0:5000/catch_me
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,13 +5,20 @@
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
 
-    if not list_of_integers:
+    if list_of_integers is None or list_of_integers == []:
         return None
-
-    retval = list_of_integers[0]
-
-    for i in list_of_integers:
-        if i > retval:
-            retval = i
-
-    return retval
+    x = 0
+    y = len(list_of_integers)
+    retval = ((y - x) // 2) + x
+    retval = int(retval)
+    if y == 1:
+        return list_of_integers[0]
+    if y == 2:
+        return max(list_of_integers)
+    if list_of_integers[retval] >= list_of_integers[retval - 1] and\
+            list_of_integers[retval] >= list_of_integers[retval + 1]:
+        return list_of_integers[retval]
+    if retval > 0 and list_of_integers[retval] < list_of_integers[retval + 1]:
+        return find_peak(list_of_integers[retval:])
+    if retval > 0 and list_of_integers[retval] < list_of_integers[retval - 1]:
+        return find_peak(list_of_integers[:retval])
